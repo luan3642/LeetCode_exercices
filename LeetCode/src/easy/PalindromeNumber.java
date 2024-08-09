65c795281735642edfacaa134b098ab50ebba059
luan3642 <luan3642@hotmail.com>
2024-08-09 18:32:46 -0300

    ...

--- LeetCode/src/easy/PalindromeNumber.java
+++ LeetCode/src/easy/PalindromeNumber.java
@@ -1,5 +1,7 @@
 package easy;
 
+import java.util.Iterator;
+
 public class PalindromeNumber {
 	public boolean isPalindromeNumber(int value) {
 
@@ -7,14 +9,41 @@ public class PalindromeNumber {
 	}
 
 	public static void main(String[] args) {
-		int value = 121;
+		Integer value = 121;
+		int j = 0;
+		int[] vetorFront = new int[2];
+		int[] vetorBack = new int[vetorFront.length];
 		
 		String numberStr = Integer.toString(value);
 		
 		for(int i = 0; i<numberStr.length(); i++) {
-			int digit = Character.getNumericValue(numberStr.charAt(i));
-			System.out.println(digit);
+			int digitFront = Character.getNumericValue(numberStr.charAt(i));
+			System.out.println(digitFront);
+			vetorFront[i] = digitFront;
+			
 		}
+		System.out.println();
+		for(int i = numberStr.length()-1; i>=0; i--) {
+			int digitFront = Character.getNumericValue(numberStr.charAt(i));
+			System.out.println(digitFront);
+			vetorBack[j] = digitFront;
+			j++;
+		}
+		
+		
+		for (int i : vetorBack) {
+			if(vetorFront[i] == vetorBack[i]) {
+				System.out.println("palindromo");
+			} else {
+				System.out.println("nao");
+			}
+		}
+		
+		
+	
+		
+		
+		
 	}
 
 }
