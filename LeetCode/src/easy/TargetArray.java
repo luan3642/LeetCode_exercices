3cdefdd3193d91d92ba29f3322556a5c45d3a771
luan3642 <luan3642@hotmail.com>
2024-08-10 09:46:44 -0300

    ...

--- LeetCode/src/easy/TargetArray.java
+++ LeetCode/src/easy/TargetArray.java
@@ -1,29 +1,40 @@
 package easy;
 
+import java.util.Random;
+
 public class TargetArray {
 	public static void main(String[] args) {
 		int array[] = {2,3,10,14};
 		int target = 0;
 		
-		for(int i = 0; i<array.length; i++) {
-			target = (int) (Math.random() * array[i]);
-		}
 		
-		System.out.println("valor: "+target);
+		Random random = new Random();
+		int indiceAletorio1 = random.nextInt(array.length);
+		
+		int indiceAleatorio2 = random.nextInt(array.length);
+		
+		int valorAleatorio = array[indiceAletorio1];
+		int valorAleatorio2 = array[indiceAleatorio2];
+		
+		target = valorAleatorio + valorAleatorio2;
+		
+		
+		System.out.println(target);
+		
+		
+		
+		System.out.println("valor target: "+target);
 		
 		int soma[] = new int[array.length];
 		int posix = 0;
 		int posiy = 0;
 		
 		for(int i = 0; i <array.length - 1; i++) {
-			soma[i] = array[i] + array[i+1];
+			soma[i] = array[indiceAletorio1] + array[indiceAleatorio2];
 			
-			if(array[i] + array[i+1] == target) {
+			if(array[indiceAletorio1] + array[indiceAleatorio2] == target) {
 				posix = i;
 				posiy = i+1;
-			}else {
-				System.out.println("Nao há nenhum array que chegue neste valor");
-				break;
 			}
 
 		}
